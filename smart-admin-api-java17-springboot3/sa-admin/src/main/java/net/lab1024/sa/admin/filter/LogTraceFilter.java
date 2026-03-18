614327e3073c3a8f596d3fd4f78ad78809c8388d
admin <admin@qq.com>
2026-03-18 15:18:20 +0800

    日志过滤器 日志记录请求路径和 耗时

--- smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/filter/LogTraceFilter.java
+++ smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/filter/LogTraceFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * Spring Boot 3 全链路日志追踪过滤器
  */
+@Slf4j
 @Component
 @Order(Integer.MIN_VALUE)
 public class LogTraceFilter extends OncePerRequestFilter {
@@ -25,9 +27,9 @@ public class LogTraceFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        long start = System.currentTimeMillis();
         try {
             String traceId = request.getHeader(TRACE_ID);
-            System.err.println(">>>>>> [TraceFilter] 拦截到请求: " + request.getRequestURI());
             if (!StringUtils.hasLength(traceId)) {
                 // 使用 ThreadLocalRandom 生成正数的随机 Long
                 // 并转为字符串存入 MDC
@@ -38,9 +40,10 @@ public class LogTraceFilter extends OncePerRequestFilter {
 
             // 关键：虽然在 Java 里是数字，但 Header 必须是字符串
             response.setHeader(TRACE_ID, traceId);
-
+            log.info("=================请求开始==================== Method {}  URI {}", request.getMethod(), request.getRequestURI());
             filterChain.doFilter(request, response);
         } finally {
+            log.info("=================请求结束==================== cost {}", System.currentTimeMillis() - start);
             MDC.remove(TRACE_ID);
         }
     }
